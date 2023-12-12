7187758f9826aa72b0015e37526adbb6b84dffd3
xlcisneros <xlcisneros03@gmail.com>
2023-12-12 14:34:53 -0800

    Update UUG.java

--- src/data/data/addables/UUG.java
+++ src/data/data/addables/UUG.java
@@ -4,12 +4,32 @@ package data.data.addables;
 //create composite parent class for objects with id field
 public abstract class UUG {
     private String id;
+    private long creationTime;
 
-    public UUG(String id){
+     public UUG(String id){
+        this.id = id;
+        this.creationTime = System.currentTimeMillis();
+    }
+
+
+    public UUG(String id, long creationTime){
+        this.id = id;
+        this.creationTime = creationTime;
+    }
+   
+    public String getId(){
+        return id;
+    }
+    
+    public void setId(String id){
         this.id = id;
     }
 
-    public String getId(){
-        return id;
+    public long getTimeMade() {
+        return creationTime;
+    }
+
+    public void setTimeMade(long time) {
+        this.creationTime = creationTime;
     }
 }
